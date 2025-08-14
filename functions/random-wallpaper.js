d1e846f9fa35f6a30ed8ae7327fcbeb2f39b4e8a
YumeHinata <72181756+YumeHinata@users.noreply.github.com>
2025-08-14 08:09:48 +0800

    更新 random-wallpaper.js
    自定义urls.txt资源位置

--- functions/random-wallpaper.js
+++ functions/random-wallpaper.js
@@ -2,7 +2,7 @@
 export async function onRequestGet(context) {
     try {
         // 1. 可配置的远程URL列表文件（支持自由调整）
-        const URL_LIST = "https://gitee.com/YumeHinata/random-wallpaper/raw/main/urls.txt";
+        const URL_LIST = context.env.URL_LIST;
         
         // 2. 获取URL列表
         const response = await fetch(URL_LIST);
